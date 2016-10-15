a8cfe361ea9d950334c9743b0afca03130286b4f
Andrii Parfeniuk <and0parf@gmail.com>
2016-10-15 20:15:19 +0300

    make the program logic real

--- js.js
+++ js.js
@@ -10,21 +10,13 @@ var
 if (price>=discTreshhold) {
     discount = Math.round(Math.random(1)*100);
     alert("you've got a discount "+discount+"%");
+    shipping = confirm("if you pay in cash free shipping availability will be checked. Pay in cash?");
+    if (shipping) {
+        distance = +prompt("enter shipping distance in km");
+        if (distance<=30) alert("Congrats! You've got a free shippping. Your total order price with discount "+price*(1-discount/100));
+        else alert("Your total order price with discount "+price*(1-discount/100)+" uah. You shipping price "+(distance-30)*5+" uah");
+    }
+    else alert("Your total order price with discount "+price*(1-discount/100));
 }
+else alert("Your total order price "+price);
 
-shipping = confirm("if you pay in cash free shipping availability will be checked. Pay in cash?");
-if (shipping) {
-    distance = +prompt("enter shipping distance in km");
-    if (distance<=30) {
-        if (price<discTreshhold) alert("Congrats! You've got a free shippping. Your total order price "+price);
-        else alert("Congrats! You've got a free shippping. You total order price with discount "+price*(1-discount/100));
-    }
-    else {
-        if (price<discTreshhold) alert("Your total order price "+price+" uah. You shipping price "+(distance-30)*5+" uah");
-        else alert("You total order price with discount "+price*(1-discount/100)+" uah. You shipping price "+(distance-30)*5+" uah");
-    }
-}
-else{
-    if (price<discTreshhold) alert("Your total order price "+price);
-    else alert("You total order price with discount "+price*(1-discount/100));
-}
